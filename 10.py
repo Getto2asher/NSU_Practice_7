00904e792ac21018fafe8dcf67cefa65b8cf4265
egor <egorstasyk@gmail.com>
2024-04-06 10:30:47 +0700

    8

--- 10.py
+++ 10.py
@@ -1,11 +1,11 @@
 n = float(input())
-cnt = 0
+cntt = 0
 
 while True:
     n1 = float(input())
     if n1 == 0:
         break
     if n1 < n:
-        cnt += 1
+        cntt += 1
     n = n1
-print(cnt)
+print(cntt)
